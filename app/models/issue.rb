ad6dad72ec9f8b65a02a14c03ceef6dc024be058
KiritiVelivela <skvelivela@gmail.com>
2017-05-27 20:03:30 +0700

    final changes

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -4,11 +4,5 @@ class Issue < ApplicationRecord
   validates :user_id, presence: true
   validates :date, presence: true
 
-  #before_save :cal_similarity
-
-  #def cal_similarity
-    @similarity = "#{@issue}".similar("#{@issues}")
-   # puts @similarity
-  #end
 
 end
